83d5f0e33882f43f0354370ccad3848861801612
Daniel <DanielHiromoto@outlook.com>
2021-06-23 17:36:26 -0400

    Server Startup coded

--- src/Driver.java
+++ src/Driver.java
@@ -10,25 +10,72 @@ import java.util.Scanner;
  */
 public class Driver {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException{
 		
-		int portNum = 0;
+		
 		int backlog = 50;
-		InetAddress ip;
-		Scanner keyboard = new Scanner(System.in);
-		//Ask for portNum specify here
+		InetAddress loopback = InetAddress.getByName("127.0.0.1");		//For testing purposes
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter port number:");
+		int portNum = input.nextInt();
 		
-		ServerSocket house = new ServerSocket(portNum);	//binds the server to a specified port
+		System.out.println("Starting Server");
+		try (ServerSocket house = new ServerSocket(portNum, backlog, loopback)){ //binds the server to a specified port
+			while(true) {
+				System.out.println("Listening for Clients");
+				Socket mailBox = house.accept();				//starts listening for incomming client requests
+				
+				InputStream letter = mailBox.getInputStream();	//Recieve byte array from client
+				InputStreamReader readingGlasses = new InputStreamReader(letter); //turn byte array into characters
+				int ID = readingGlasses.read();	//Read from the byte array
+				System.out.println("Input from Client: " + ID);
+				proccessCommand(mailBox, ID);	//Handle client request
+				//Process input
+				/* Commands:
+				 * QUIT(-1, null, "Quit"),
+					DATE_AND_TIME(0, "free", "Date and Time"), 
+					UPTIME(1, "uptime", "Uptime"), 
+					MEMORY_USAGE(2, "free", "Memory Usage"), 
+					NETSTAT(3, "netstat", "Netstat"), 
+					CURRENT_USERS(4, "w", "Current Users"), 
+					RUNNING_PROCESSES(5, "ps", "Running Processes");
+				 */
+				
+				mailBox.close();//close client connection
+			}//End while loop	
+		} catch (IOException ex) {
+			System.out.println("Server Exception" + ex.getMessage());
+			ex.printStackTrace();
+		}
 		
-		Socket mailBox = house.accept();				//starts listening for incomming client requests
 		
-		InputStream letter = mailBox.getInputStream();	//Recieve byte array from client
-		InputStreamReader readingGlasses = new InputStreamReader(letter); //turn byte array into characters
+	}
+	
+	static public void proccessCommand(Socket outBox, int ID) throws IOException {
+		OutputStream output = outBox.getOutputStream();	//get the return address
 		
-		//Process input
-		
-		mailBox.close();
-		
+		switch(ID) {
+		case -1:
+			return;
+		case 0:
+			//Date and Time
+			break;
+		case 1:
+			//Updtime
+			break;
+		case 2:
+			//Memory_usage
+			break;
+		case 3:
+			//Netstat
+			break;
+		case 4:
+			//Current_Users
+			break;
+		case 5:
+			//Running Processes
+			break;
+		}
 	}
 
 }
