6edd4d91c0fe9ae18a7a17c6135eadcd938cb78f
Daniel <DanielHiromoto@outlook.com>
2021-06-23 17:07:32 -0400

    Started Creating the Structure

--- src/Driver.java
+++ src/Driver.java
@@ -1,12 +1,34 @@
 import java.io.*;
 import java.net.*;
 import java.util.Date;
+import java.util.Scanner;
 
+
+/*
+ * - Listen for client requests on the specified network address and port
+ * 
+ */
 public class Driver {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		
+		int portNum = 0;
+		int backlog = 50;
+		InetAddress ip;
+		Scanner keyboard = new Scanner(System.in);
+		//Ask for portNum specify here
+		
+		ServerSocket house = new ServerSocket(portNum);	//binds the server to a specified port
+		
+		Socket mailBox = house.accept();				//starts listening for incomming client requests
+		
+		InputStream letter = mailBox.getInputStream();	//Recieve byte array from client
+		InputStreamReader readingGlasses = new InputStreamReader(letter); //turn byte array into characters
+		
+		//Process input
+		
+		mailBox.close();
+		
 	}
 
 }
